6a38a225f51134fb5b372172c52a10b9c80c1959
Julian Biebelberg <jbiebelberg@gmail.com>
2024-11-27 22:37:14 -0500

    Update app.js
    Changed CORS logic

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const port = process.env.PORT || 3001;
-app.use(function (req, res, next) {
+app.use(cors());
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
